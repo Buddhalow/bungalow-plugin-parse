1ea57ff9fb31c88e9eb263bb8000d628631b5d8f
Dr. Sounds <management@dr-sounds.com>
2017-12-06 21:17:24 +0000

    Commit

--- datasources/parsedatasource.js
+++ datasources/parsedatasource.js
@@ -1,90 +1,54 @@
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 define(['controls/datasource'], function (SPDataSource) {
     return class SPParseDataSource extends SPDataSource {
         constructor(model, fields) {
             super();
             this.fields = fields;
-            this.model = model;
-            
         }
-        async getObjectById(id) {
-            return await new Promise((resolve, reject) => {
-                let query = new Parse.Query(this.Model);
-                this.fields.filter(f => f.type === 'manyToOne').map(f => {
-                    query = query.include(f.id);    
-                });
-                query.get(id, {
-                    success: (obj) => {
-                        resolve(obj.simplify());
-    
-                   }, error: (e) => {
+        async request(method, uri, options, data) {
+            var model = capitalizeFirstLetter(uri.split(':')[1]);
+            var id = uri.split(':')[2];
+            if (/^parse:([a-zA-Z0-9]+):([a-zA-Z0-9]+)$/g.test(uri)) {
+                if (method == 'DELETE') {
                     
-                   }
-                });
-            });
-        }
-        async find(q) {
-           return await new Promise((resolve, reject) => {
-                let query = new Parse.Query(this.Model);
-                if (q instanceof String ) {
-                            
-                    query =  query.contains('name', q);
-                } else if (q instanceof Object) {
-                    for (let k in Object.keys(q)) {
-                        query = query.contains(k, q[k]);
-                    }
-                }
-                query.find({
-                    success: (objs) => {
-                        resolve(objs.map(o => o.simplify()));
-    
-                   }, error: (e) => {
-                    throw e;
-                   }
-                });
-            });
-        }
-        get model() {
-            return this._model;
-        }
-        set model(value) {
-            this._model = value;
-            this.Model = Parse.Object.extend(value);
-        }
-        get numberOfFields() {
-            return Object.keys(this.fields).length;
-        }
-        getFieldByIndex(index) {
-            return Object.values(this.fields)[index];
-        }
-        async saveOrUpdate(data) {
-            return new Promise((resolve, fail) => {
-                if (!!data.id) {
+                } else if (method == 'PUT') {
                     let q = new Parse.Query(this.Model);
-                    q.get(data.id, {
-                        success: (obj) => {
-                            for (let f of this.fields) {
-                                let k = f.id;
-                                let val = data[k];
-                                if (f.type == 'number') {
-                                    val = parseInt(val); 
-                                } else if (f.type == 'datetime-local') {
-                                    val = new Date(val);
-                                } else if (val instanceof Object) {
-                                    val = {"__type": "Pointer", "className": f.model.id, "objectId": val.id};
-                                }
-                                obj.set(k, val);
-                                obj.save({
-                                    success: (obj) => {
-                                        resolve(obj);
-                                    }, error: (e) => {
-                                        throw e;
-                                    }
-                                });
-                            }
+                    let obj = await q.get(data.id);
+                    for (let f of this.table.fields) {
+                        let k = f.id;
+                        let val = data[k];
+                        if (f.type == 'number') {
+                            val = parseInt(val); 
+                        } else if (f.type == 'datetime-local') {
+                            val = new Date(val);
+                        } else if (val instanceof Object) {
+                            val = {"__type": "Pointer", "className": f.model.id, "objectId": val.id};
                         }
-                    })
+                        obj.set(k, val);
+                        obj.save({
+                            success: (obj) => {
+                                resolve(obj);
+                            }, error: (e) => {
+                                throw e;
+                            }
+                        });
+                    }
                 } else {
-                    let obj = new this.Model();
+                
+                    let query = new Parse.Query(model);
+                    this.fields.filter(f => f.type === 'manyToOne').map(f => {
+                        query = query.include(f.id);    
+                    });
+                    let result = await query.get(id).simplify();
+                    return result;
+                }
+            }
+            if (/^parse:([a-zA-Z0-9]+)$/g.test(uri)) {
+                if (method == 'POST') {
+                    let obj = new Parse.Object(model);
                     for (let f of this.fields) {
                         let k = f.id;
                         let val = data[k];
@@ -98,20 +62,26 @@ define(['controls/datasource'], function (SPDataSource) {
                         
                         obj.set(k, val);
                     }
-                    obj.save({
-                        error: (obj, error) => {
-                            debugger;
-                            throw error;
-                            fail({
-                                error: 'error'
-                            });
-                        },
-                        success: (savedObject) => {
-                            resolve(savedObject.simplify())
+                    let result = await obj.save();
+                    return result.simplify();
+                } else {
+                    let query = new Parse.Query(model);
+                    if (options instanceof String ) {
+                                
+                        query =  query.contains('name', options);
+                    } else if (options instanceof Object) {
+                        for (let k in Object.keys(options)) {
+                            let str = options[k];
+                            if (str instanceof String) {
+                            query = query.contains(k, str);
+                            }
                         }
-                    })
+                    }
+                    let result = await query.find().then((t) => {objects: t});
+                    result.objects = result.objects.map(o => o.simplify());
+                    return result;
                 }
-            });
+            }
         }
         
     }
--- views/model.js
+++ views/model.js
@@ -2,17 +2,79 @@ define(
     [
         'controls/tabledatasource', 
         'controls/tabledesigner',
+        'plugins/parse/datasources/parsedatasource',
         'controls/view'
     ], function (
         SPTableDataSource, 
         SPTableDesigner, 
+        SPParseDataSource,
         SPViewElement
     ) {
     return class SPModelViewElement extends SPViewElement {
+            get dataSource() {
+                return this.listView.dataSource;
+            }
+            set dataSource(value) {
+                this.listView.dataSource = value;
+                this.form.dataSource = value;
+            }
+            get dataSource() {
+                return this.listView.dataSource;
+            }
+            set designer(value) {
+                this.listView.designer = value;
+            }
+            get designer() {
+                return this.listView.designer;
+            }
             createdCallback() {
                 super.createdCallback();
                 this.content = document.createElement('div');
                 this.classList.add('sp-view');
+                this.section = document.createElement('sp-tabcontent');
+                this.editSection = document.createElement('sp-tabcontent');
+                this.section.setAttribute('data-tab-id', 'overview');
+                this.appendChild(this.section);
+                
+                
+                this.header = document.createElement('sp-header');
+                this.header.setAttribute('size', 128);
+                
+                this.listView =  document.createElement('sp-table');
+                this.section.appendChild(this.header);
+                if (this.content instanceof Node)
+                this.section.appendChild(this.content);
+                this.containerElement = document.createElement('div');
+                this.containerElement.classList.add('container');
+                this.section.appendChild(this.containerElement);
+                this.containerElement.appendChild(this.listView);
+                this.listView.header = this.header;
+                this.listView.view = this;
+                this.listView.emptyText = this.emptyText;
+                var self = this;
+                this.listView.delegate = {
+                    onRowDoubleClick(row, obj) {
+                        var dialog = document.createElement('sp-modal');
+                            dialog.label = _e('Edit') + ' ' + this.model;
+                            
+                        document.body.appendChild(dialog);
+                        dialog.navigate('parse:' + self.model.toLocaleLowerCase() + ':' + obj.id);
+                        dialog.show();
+                    },
+                    onRowSingleClick() {
+                        
+                    }
+                };
+                this.containerElement = document.createElement('div');
+                this.containerElement.classList.add('container');
+                this.editSection.appendChild(this.containerElement);
+                this.form = document.createElement('sp-form');
+                this.form.label = _e('Edit') + ' ' + this.model;
+                this.containerElement.appendChild(this.form);
+                this.editSection.style.display = 'none';
+               
+                    
+                
             }
             activate() {
                 super.activate();
@@ -23,13 +85,12 @@ define(
                         name: _e(this.label)
                     }],
                     add: {
-                        uri: 'bungalow:' + this.model.toLocaleLowerCase() + ':add'
+                        uri: 'parse:' + this.model.toLocaleLowerCase() + ':add'
                     }
                 }); 
                 } catch (e) {
                     
                 }
-                this.created = true;
             }
             get type() {
                 return this.getAttribute('type');
@@ -57,19 +118,15 @@ define(
             }
             attributeChangedCallback(attrName, oldVal, newVal) {
                 if (attrName === 'uri') {
+                    
                     this.innerHTML = '';
-                    this.section = document.createElement('sp-tabcontent');
-                    this.section.setAttribute('data-tab-id', 'overview');
-                    this.appendChild(this.section);
-                    
-                    if (newVal === 'bungalow:' + this.model.toLowerCase()) {
-                    
-                        this.header = document.createElement('sp-header');
-                        this.header.setAttribute('size', 128);
+                    newVal = 'parse:' + newVal.split(':').splice(1).join(':');
+                    if (newVal === 'parse:' + this.model.toLowerCase()) {
+    
                         this.header.setState({
                             type: this.type,
                             name: _e(this.label),
-                            uri: 'bungalow:' + this.model.toLowerCase(),
+                            uri: 'parse:' + this.model.toLowerCase(),
                             type: this.model,
                             description: this.description,
                             buttons: [{
@@ -80,64 +137,21 @@ define(
                                     dialog.label = _e('Add') + ' ' + this.model;
                                     document.body.appendChild(dialog);
                                       dialog.show();
-                                  dialog.navigate('bungalow:' + self.model.toLocaleLowerCase() + ':add');
+                                  dialog.navigate('parse:' + self.model.toLocaleLowerCase() + ':add');
                                   
                                 }
                             }]
                         });
-                        GlobalTabBar.setState({
-                            objects: [{
-                                id: 'overview',
-                                name: _e(this.label)
-                            }],
-                            add: {
-                                uri: 'bungalow:' + this.model.toLocaleLowerCase() + ':add'
-                            }
-                        }); 
-                        this.listView =  document.createElement('sp-table');
-                        this.section.appendChild(this.header);
-                        if (this.content instanceof Node)
-                        this.section.appendChild(this.content);
-                        this.containerElement = document.createElement('div');
-                        this.containerElement.classList.add('container');
-                        this.section.appendChild(this.containerElement);
-                        this.containerElement.appendChild(this.listView);
-                        this.listView.header = this.header;
-                        this.listView.view = this;
-                        this.listView.designer = this.tableDesigner;    
-                        this.listView.dataSource = this.tableDataSource;
-                        this.listView.negative = this.negative;
-                        this.listView.emptyText = this.emptyText;
-                        this.tableDataSource.fetchNext();
-                        var self = this;
-                        this.listView.delegate = {
-                            onRowDoubleClick(row, obj) {
-                                var dialog = document.createElement('sp-modal');
-                                    dialog.label = _e('Edit') + ' ' + this.model;
-                                    
-                                document.body.appendChild(dialog);
-                                dialog.navigate('bungalow:' + self.model.toLocaleLowerCase() + ':' + obj.id);
-                                dialog.show();
-                            },
-                            onRowSingleClick() {
-                                
-                            }
-                        };
-                        this.created = true;
+                        this.listView.setAttribute('uri', newVal);
                     } else {
-                        this.containerElement = document.createElement('div');
-                        this.containerElement.classList.add('container');
-                        this.appendChild(this.containerElement);
-                        this.form = document.createElement('sp-form');
-                        this.form.dataSource = this.formDataSource;
-                        this.form.label = _e('Edit') + ' ' + this.model;
-                        this.containerElement.appendChild(this.form);
+                        this.section.style.display = 'none';
+                        this.editSection.style.display = 'block';
                         let uri = newVal.split(':');
                         let id = uri[2];
                         this.form.setAttribute('data-object-id', id);
                         
                     }
-                } 
+                }
             }
             refresh() {
                 let uri =this.getAttribute('uri');
